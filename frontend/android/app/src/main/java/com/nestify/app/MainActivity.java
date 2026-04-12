3e75d105504c470765e353082c5d89fb39ce17dc
S1nuxoff <s1nuxoff@gmail.com>
2026-04-12 08:53:09 +0200

    chore: remove legacy apps from repository

--- frontend/android/app/src/main/java/com/nestify/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.nestify.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- frontend/src/components/skeleton/SkeletonFeatured.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "../../styles/Skeleton.css";
-
-export default function SkeletonFeatured() {
-  return <div className="skeleton skel-featured" />;
-}
